f9d49e651a998b7e652ecb8cf1dafc03e9016d0a
jootaef <jaydanv100@gmail.com>
2024-06-07 00:00:20 -0500

    final change

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,10 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video  id='hero-video' autoplay loop muted> 
-      <source src='/videos/video-2.mp4' type='video/mp4'></source>
-      </video>
-      <img id='hero-image' src='../images/pantalla-1.jpg' alt='Hero '></img>
+      <video src='/videos/video-2.mp4' autoPlay loop muted />
       <h1>Bienvenido a InfoRuedas</h1>
       <p>Encuentra toda la información técnica que necesitas sobre autos</p>
       <div className='hero-btns'>
